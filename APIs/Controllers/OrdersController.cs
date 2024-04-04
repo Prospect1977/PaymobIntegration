86159005a1a9d2a98d8198676b03d08ac668a03b
mohamed essm <langara5000@hotmail.com>
2024-04-04 20:54:57 +0200

    final

---
+++ APIs/Controllers/OrdersController.cs
@@ -0,0 +1,62 @@
+﻿using APIs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebProject.Data;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private ApplicationDbContext db;
+
+        public OrdersController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        [HttpPost]
+        [Route("NewOrder")]
+        public async Task<IActionResult> NewOrder(OrderModel userOrder)
+        {
+            ResponseModel resp = new ResponseModel();
+
+
+            var myOrder = new WebProject.Data.UserOrder
+            {
+                UserId = userOrder.UserId,
+                ProductId = userOrder.ProductId,
+                UnitPrice = userOrder.UnitPrice,
+                Quantity = userOrder.Quantity,
+                Payment = userOrder.Payment,
+                NetPayment = 0,
+                DataDate = DateTime.Now,
+                Source = userOrder.Source,
+                OrderId = userOrder.OrderId,
+            };
+            if (userOrder.TransactionId != null)//kiosk
+            {
+                myOrder.TransactionId = userOrder.TransactionId;
+            }
+            db.UserOrders.Add(myOrder);
+            db.SaveChanges();
+            resp.status = true;
+            resp.data = myOrder.Id;
+            return Ok(resp);
+
+        }
+
+        public class OrderModel
+        {
+            public string UserId { get; set; }
+            public int ProductId { get; set; }
+            public double UnitPrice { get; set; }
+            public int Quantity { get; set; }
+            public double Payment { get; set; }
+            public string Source { get; set; }
+            public int OrderId { get; set; }
+            public int? TransactionId { get; set; }
+
+        }
+    }
+}
---
+++ APIs/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using APIs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebProject.Data;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext db;
+
+        public ProductsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        [Route("GetProducts")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var AllProducts = db.Products;
+            ResponseModel resp = new ResponseModel { status = true, data = AllProducts };
+            return Ok(resp);
+        }
+    }
+}
